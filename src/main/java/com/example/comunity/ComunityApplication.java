c3a6fe3e62b5a1b7133477d7a3f88731ce0def9a
shinjunseok <sjsage522@gmail.com>
2021-02-23 16:17:48 +0900

    initial commit

---
+++ src/main/java/com/example/comunity/ComunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.comunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comunity/ComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
