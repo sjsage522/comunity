20747e02fda4af265d001d743bbfbce3ce563a74
shinjunseok <sjsage522@gmail.com>
2021-03-03 18:29:24 +0900

    modified : Logger Class
    컨테이너에서 관리하도록 변경

--- src/main/java/com/example/comunity/config/LoadDatabase.java
+++ src/main/java/com/example/comunity/config/LoadDatabase.java
@@ -3,22 +3,31 @@ package com.example.comunity.config;
 import com.example.comunity.domain.User;
 import com.example.comunity.dto.user.UserJoinDto;
 import com.example.comunity.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class LoadDatabase {
+    private final DefaultListableBeanFactory df;
 
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+    @Bean
+    public Logger log() {
+        return LoggerFactory.getLogger(LoadDatabase.class);
+    }
 
     @Bean
     CommandLineRunner initDatabase(UserService userService) {
         return new CommandLineRunner() {
+            final Logger log = log();
+
             @Override
-            public void run(String... args) throws Exception {
+            public void run(final String... args) throws Exception {
                 User newUser = User.createUser("Junseok1234", "junseok", "jun", "1234", "junseok@test.com");
                 log.info("Preloading { userId : [" + userService
                         .join(
@@ -30,6 +39,9 @@ public class LoadDatabase {
                                         newUser.getEmail()
                                 )
                         ) + "] } CommandLineRunner...");
+
+                Logger logger = df.getBean(Logger.class);
+                log.info("logger = " + logger);
             }
         };
     }
