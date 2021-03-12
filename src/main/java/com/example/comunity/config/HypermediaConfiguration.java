810435af8b6a3556d5f68518e66b8ac551dea360
shinjunseok <sjsage522@gmail.com>
2021-03-12 11:41:32 +0900

    add : comment
    HypermediaConfiguration.java 주석 추가

--- src/main/java/com/example/comunity/config/HypermediaConfiguration.java
+++ src/main/java/com/example/comunity/config/HypermediaConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 import static org.springframework.hateoas.config.EnableHypermediaSupport.*;
 
+/**
+ * affordance ->  to take this,
+ * using a different hypermedia-based media type
+ *
+ * To switch to HAL-FORMS, create this codes.
+ */
 @Configuration
 @EnableHypermediaSupport(type = HypermediaType.HAL_FORMS)
 public class HypermediaConfiguration {
