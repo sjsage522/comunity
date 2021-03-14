b06e91df7f156cfcebb26436468d542720916726
shinjunseok <sjsage522@gmail.com>
2021-03-15 01:27:56 +0900

    add : file service 계층
    file 단건 조회기능 추가

---
+++ src/main/java/com/example/comunity/service/FileService.java
@@ -0,0 +1,24 @@
+package com.example.comunity.service;
+
+import com.example.comunity.domain.UploadFile;
+import com.example.comunity.exception.NoMatchFileInfoException;
+import com.example.comunity.repository.FileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.File;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class FileService {
+
+    private final FileRepository fileRepository;
+
+    public UploadFile findById(final Long id) {
+        UploadFile findFile = fileRepository.findById(id);
+        if (findFile == null) throw new NoMatchFileInfoException("존재하지 않는 파일입니다.");
+        return findFile;
+    }
+}
