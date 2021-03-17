10086dfd618fb38715fa1a13dace0ef1d5a2298e
shinjunseok <sjsage522@gmail.com>
2021-03-17 12:18:28 +0900

    update : 조회성능 최적화
    컬렉션 조회 시 지연로딩이 되는 부분 최적화.
    (페이징 처리 때문에 xToMany 관계의 fetch join은 불가능)

--- src/main/java/com/example/comunity/repository/board/BoardRepository.java
+++ src/main/java/com/example/comunity/repository/board/BoardRepository.java
@@ -13,13 +13,31 @@ public interface BoardRepository extends JpaRepository<Board, Long>, BoardReposi
 
     /**
      * jpql fetch join
-     * @EntityGraph 사용 대신, Count 쿼리를 명시적으로 생성
      *
      * @param pageable .
+     * @EntityGraph 사용 대신, Count 쿼리를 명시적으로 생성
      */
-    @Query(value = "select b from Board b join fetch b.category c where c.categoryName = :categoryName order by b.boardId desc",
+    @Query(value = "select b " +
+            "from Board b " +
+            "join fetch b.category c " +
+            "join fetch b.user u " +
+            "where c.categoryName = :categoryName " +
+            "order by b.boardId desc",
             countQuery = "select count(b) from Board b join b.category c where c.categoryName = :categoryName")
     Page<Board> findAllWithCategory(@Param("categoryName") final String categoryName, final Pageable pageable);
 
+
+    /**
+     * uploadFiles는 fetch join하면 안됨!
+     * 일대다 관계에서 fetch join시 페이징을 할 수 없음 (데이터의 정합성)
+     *
+     * @param pageable .
+     */
+    @Query(value = "select b " +
+            "from Board b " +
+            "join fetch b.category c " +
+            "join fetch b.user u " +
+            "order by b.boardId desc",
+            countQuery = "select count(b) from Board b")
     Page<Board> findAllByOrderByBoardIdDesc(final Pageable pageable);
 }
