e133b00856a4dcf613c82f5478cc1341e870f4f4
shinjunseok <sjsage522@gmail.com>
2021-03-17 15:29:42 +0900

    update : method name
    findById -> findCommentById

--- src/main/java/com/example/comunity/service/CommentService.java
+++ src/main/java/com/example/comunity/service/CommentService.java
@@ -9,12 +9,14 @@ import com.example.comunity.exception.NoMatchBoardInfoException;
 import com.example.comunity.exception.NoMatchCommentInfoException;
 import com.example.comunity.exception.NoMatchUserInfoException;
 import com.example.comunity.repository.board.BoardRepository;
-import com.example.comunity.repository.CommentRepository;
+import com.example.comunity.repository.comment.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -35,7 +37,7 @@ public class CommentService {
 
         Long parentId = commentApplyDto.getParentId();
         if (parentId != null) {
-            Comment parentComment = commentRepository.findById(parentId);
+            Comment parentComment = commentRepository.findCommentById(parentId);
             if (parentComment == null) throw new NoMatchCommentInfoException("존재하지 않는 답글입니다.");
             parentComment.addChildComment(comment);
         }
@@ -44,22 +46,22 @@ public class CommentService {
     }
 
     public Comment findById(final Long id) {
-        return commentRepository.findById(id);
+        return commentRepository.findCommentById(id);
     }
 
     @Transactional
     public void delete(final User loginUser, final Long commentId) {
-        Comment findComment = commentRepository.findById(commentId);
+        Comment findComment = commentRepository.findCommentById(commentId);
 
         checkValid(loginUser, findComment);
 
-        commentRepository.delete(findComment);
+        commentRepository.delete(commentId);
     }
 
     @Transactional
     public Comment update(User loginUser, Long id, CommentUpdateDto commentUpdateDto) {
 
-        Comment findComment = commentRepository.findById(id);
+        Comment findComment = commentRepository.findCommentById(id);
 
         checkValid(loginUser, findComment);
 
@@ -75,7 +77,8 @@ public class CommentService {
             throw new NoMatchUserInfoException("다른 사용자의 답글을 삭제할 수 없습니다.");
     }
 
-    public List<Comment> findAll(final Long boardId) {
-        return commentRepository.findAll(boardId);
+    public List<Comment> findAll(final Long boardId, final Integer pageNumber) {
+        return commentRepository.findAll(boardId, PageRequest.of(pageNumber, 2)).stream()
+                .collect(Collectors.toList());
     }
 }
