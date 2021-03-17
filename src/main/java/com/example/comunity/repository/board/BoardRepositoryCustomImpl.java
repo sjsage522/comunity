636de65d8059cc626832e0a359d0b33f412ff315
shinjunseok <sjsage522@gmail.com>
2021-03-17 12:16:00 +0900

    update : 조회성능 최적화
    게시글 단건조회 시 지연로딩이 되는 부분을 fetch join으로 한방에 조회하도록 변경

--- src/main/java/com/example/comunity/repository/board/BoardRepositoryCustomImpl.java
+++ src/main/java/com/example/comunity/repository/board/BoardRepositoryCustomImpl.java
@@ -43,6 +43,7 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
 
     /**
      * 특정 카테고리에 포함된 하나의 게시판 조회 (게시판 번호로)
+     * 일대다 패치조인 포함 (boardId는 겹칠 수 없기 떄문에 distinct 불필요)
      *
      * @param boardId      조회할 게시판의 번호
      * @param categoryName 조회할 게시판이 포함된 카테고리 이름
@@ -52,7 +53,9 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
         try {
             return em.createQuery(
                     "select b from Board b" +
+                            " join fetch b.user" +
                             " join fetch b.category c" +
+                            " join fetch b.uploadFiles" +
                             " where b.boardId = :boardId" +
                             " and c.categoryName = :categoryName", Board.class)
                     .setParameter("boardId", boardId)
@@ -63,34 +66,5 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
             System.out.println("nre(message) = " + message);
             return null;
         }
-    }
-
-    /**
-     * 사용자가 작성한 게시판 전부 조회
-     */
-    public List<Board> findAllWithUser(final String userId) {
-        return em.createQuery(
-                "select b from Board b" +
-                        " join fetch b.user u" +
-                        " where u.userId = :userId", Board.class)
-                .setParameter("userId", userId)
-                .getResultList();
-    }
-
-    /**
-     * 사용자가 작성한 특정 카테고리에 포함된 게시판 전부 조회
-     */
-    public List<Board> findBoardByIdWithCategoryAndUser(final Long boardId, final String categoryName, final String userId) {
-        return em.createQuery(
-                "select b from Board b" +
-                        " join fetch b.category c" +
-                        " join fetch b.user u" +
-                        " where b.boardId = :boardId" +
-                        " and c.categoryName = :categoryName" +
-                        " and u.userId = :userId", Board.class)
-                .setParameter("boardId", boardId)
-                .setParameter("categoryName", categoryName)
-                .setParameter("userId", userId)
-                .getResultList();
     }
 }
