8f12845ccbb11d3f4683d771da3e59d70df564d5
shinjunseok <sjsage522@gmail.com>
2021-05-10 16:42:46 +0900

    add : comment repo test
    테스트 코드 틀 작성.

---
+++ src/test/java/com/example/comunity/repository/comment/CommentRepositoryCustomImplTest.java
@@ -0,0 +1,28 @@
+package com.example.comunity.repository.comment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentRepositoryCustomImplTest {
+
+    @Test
+    void apply() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findCommentById() {
+    }
+
+    @Test
+    void deleteAllByIds() {
+    }
+}
---
+++ src/test/java/com/example/comunity/repository/comment/CommentRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.comunity.repository.comment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentRepositoryTest {
+
+    @Test
+    void findAll() {
+    }
+}
