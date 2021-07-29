97a6bbe63e9dc8b887bec2fa9da22029761ab2b5
shinjunseok <sjsage522@gmail.com>
2021-07-29 14:23:38 +0900

    add : repository test
    - 게시글, 답글 레포지토리 단위 테스트 작성
    - 테스트 리소스 추가

---
+++ src/test/java/com/example/comunity/repository/BoardRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.example.comunity.repository;
+
+import com.example.comunity.domain.Board;
+import com.example.comunity.domain.Category;
+import com.example.comunity.domain.CategoryName;
+import com.example.comunity.domain.User;
+import com.example.comunity.exception.NoMatchBoardInfoException;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("레포지토리 테스트 (board)")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@DirtiesContext
+class BoardRepositoryTest {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @BeforeEach
+    void before(@Autowired EntityManager em) {
+        User user = User.of("testId", "tester", "testNickname", "1234", "tester@gmail.com");
+        Category codingCategory = Category.from("coding");
+        Category gameCategory = Category.from("game");
+        em.persist(user);
+        em.persist(codingCategory);
+        em.persist(gameCategory);
+
+        em.persist(Board.of(user, codingCategory, "title1", "content1"));
+        em.persist(Board.of(user, codingCategory, "title2", "content2"));
+        em.persist(Board.of(user, codingCategory, "title3", "content3"));
+        em.persist(Board.of(user, gameCategory, "title4", "content4"));
+        em.persist(Board.of(user, gameCategory, "title5", "content5"));
+        em.persist(Board.of(user, gameCategory, "title6", "content6"));
+    }
+
+    @Test
+    @DisplayName("게시글 아이디와 카테고리 이름으로 게시글 단건 조회 성공 테스트")
+    void _01_findByBoardIdAndCategoryName_succeed_test() {
+        Board findBoard = boardRepository.findByBoardIdAndCategoryName(6L, CategoryName.game)
+                .orElseThrow(NoMatchBoardInfoException::new);
+
+
+        assertThat(findBoard.getTitle()).isEqualTo("title6");
+        assertThat(findBoard.getContent()).isEqualTo("content6");
+    }
+
+    @Test
+    @DisplayName("게시글 아이디 리스트로 게시글들 삭제 성공 테스트")
+    void _02_deleteWithIds_succeed_test() {
+        System.out.println(boardRepository.findAll());
+        boardRepository.deleteWithIds(List.of(7L, 8L, 9L));
+
+        List<Board> all = boardRepository.findAll();
+        assertThat(all.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("카테고리 이름으로 전체 게시글 페이징 조회 성공 테스트")
+    void _03findAllByCategoryNameWithPaging_succeed_test() {
+        List<Board> boardList = boardRepository.findAllByCategoryNameWithPaging(CategoryName.coding, PageRequest.of(0, 2))
+                .stream()
+                .collect(Collectors.toList());
+
+        assertThat(boardList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("전체 게시글 페이징 조회 성공 테스트")
+    void _04_findAllWithPaging_succeed_test() {
+        List<Board> onePageList = boardRepository.findAllWithPaging(PageRequest.of(0, 5))
+                .stream()
+                .collect(Collectors.toList());
+
+        List<Board> twoPageList = boardRepository.findAllWithPaging(PageRequest.of(1, 5))
+                .stream()
+                .collect(Collectors.toList());
+
+        assertThat(onePageList.size()).isEqualTo(5);
+        assertThat(twoPageList.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사용자 아이디로 모든 게시글 조회 성공 테스트")
+    void _05_findAllByUser_UserId_succeed_test() {
+        List<Board> boardList = boardRepository.findAllByUser_UserId("testId");
+
+        assertThat(boardList.size()).isEqualTo(6);
+    }
+}
---
+++ src/test/java/com/example/comunity/repository/CommentRepositoryTest.java
@@ -0,0 +1,111 @@
+package com.example.comunity.repository;
+
+import com.example.comunity.domain.Board;
+import com.example.comunity.domain.Category;
+import com.example.comunity.domain.Comment;
+import com.example.comunity.domain.User;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("레포지토리 테스트 (comment)")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@DirtiesContext
+class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    void before() {
+        User user = User.of("testId", "tester", "testNickname", "1234", "tester@gmail.com");
+        Category codingCategory = Category.from("coding");
+        Board board = Board.of(user, codingCategory, "title", "content");
+
+        em.persist(user);
+        em.persist(codingCategory);
+        em.persist(board);
+
+        Comment parentComment1 = Comment.of(user, board, "parentComment1");
+        em.persist(parentComment1); //count: 1
+
+        Comment parentComment2 = Comment.of(user, board, "parentComment2");
+        em.persist(parentComment2); //count: 2
+        em.persist(Comment.of(user, board, "parentComment3")); //count : 3
+
+        Comment childComment1 = Comment.of(user, board, "childComment1");
+        em.persist(childComment1); //count: 4
+
+        Comment childComment2 = Comment.of(user, board, "childComment2");
+        em.persist(childComment2); //count: 5
+
+        Comment childComment3 = Comment.of(user, board, "childComment3");
+        em.persist(childComment3); //count: 6
+
+        parentComment1.addChildComment(childComment1);
+        parentComment1.addChildComment(childComment2);
+        parentComment2.addChildComment(childComment3);
+    }
+
+    @Test
+    @DisplayName("게시글 아이디로 모든 답글 조회 성공 테스트")
+    void _01_findAllByBoardIdIfParentIsNullWithPaging_succeed_test() {
+        final List<Comment> all = commentRepository.findAll();
+        final Comment comment = all.get(0);
+        System.out.println(comment.getBoard().getId());
+
+        List<Comment> commentList = commentRepository.findAllByBoardId(1L);
+
+        assertThat(commentList.size()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("게시글 아이디로 부모답글이 없는 모든 답글 페이징 조회 성공 테스트")
+    void _02_findAllByBoardIdIfParentIsNullWithPaging_succeed_test() {
+        List<Comment> parentCommentList = commentRepository.findAllByBoardIdIfParentIsNullWithPaging(2L, PageRequest.of(0, 2))
+                .stream()
+                .collect(Collectors.toList());
+
+        Comment parentComment1 = parentCommentList.get(0);
+        Comment parentComment2 = parentCommentList.get(1);
+
+        List<Comment> comment1Children = parentComment1.getChildren();
+        List<Comment> comment2Children = parentComment2.getChildren();
+
+        assertThat(parentCommentList.size()).isEqualTo(2);
+        assertThat(comment1Children.size()).isEqualTo(2);
+        assertThat(comment2Children.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("답글 아이디 리스트로 게시글들 삭제 성공 테스트")
+    void _03_deleteWithIds_succeed_test() {
+        System.out.println(commentRepository.findAll());
+
+        //given
+        commentRepository.deleteWithIds(List.of(13L, 14L, 15L, 16L, 17L, 18L));
+
+        //when
+        List<Comment> all = commentRepository.findAll();
+
+        //then
+        assertThat(all.size()).isEqualTo(0);
+    }
+}
+
+
