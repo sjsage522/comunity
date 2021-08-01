14d0e4593827ec4c98b1ca52841b21443ddf6c0b
shinjunseok <sjsage522@gmail.com>
2021-08-01 19:58:38 +0900

    update : user dto
    빌더 패턴 적용

--- src/main/java/com/example/comunity/dto/user/UserJoinRequest.java
+++ src/main/java/com/example/comunity/dto/user/UserJoinRequest.java
@@ -3,6 +3,8 @@ package com.example.comunity.dto.user;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Getter
 public class UserJoinRequest {
@@ -21,4 +23,73 @@ public class UserJoinRequest {
 
     @NotBlank(message = "이메일은 필수입니다.")
     private String email;
+
+    public static class Builder {
+        /* 필수 매개 변수 */
+        private String userId;
+        private String name;
+        private String nickName;
+        private String password;
+        private String email;
+        /*            */
+
+        public Builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder nickName(String nickName) {
+            this.nickName = nickName;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserJoinRequest build() {
+            return new UserJoinRequest(this);
+        }
+    }
+
+    private UserJoinRequest(Builder builder) {
+        checkValid(builder.userId, builder.name, builder.nickName, builder.password, builder.email);
+        this.userId = builder.userId;
+        this.name = builder.name;
+        this.nickName = builder.nickName;
+        this.password = builder.password;
+        this.email = builder.email;
+    }
+
+//    private UserJoinRequest(String userId, String name, String nickName, String password, String email) {
+//        checkValid(userId, name, nickName, password, email);
+//        this.userId = userId;
+//        this.name = name;
+//        this.nickName = nickName;
+//        this.password = password;
+//        this.email = email;
+//    }
+//
+//    public static UserJoinRequest from(String userId, String name, String nickName, String password, String email) {
+//        return new UserJoinRequest(userId, name, nickName, password, email);
+//    }
+
+    private void checkValid(String... args) {
+        Arrays.stream(args)
+                .filter(Objects::isNull)
+                .forEach(arg -> {
+                    throw new IllegalArgumentException("user join dto [arguments] must be not null.");
+                });
+    }
 }
--- src/main/java/com/example/comunity/dto/user/UserUpdateRequest.java
+++ src/main/java/com/example/comunity/dto/user/UserUpdateRequest.java
@@ -7,12 +7,43 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class UserUpdateRequest {
 
-    @NotBlank(message = "변경할 이름을 입력해 주세요.")
     private String name;
 
-    @NotBlank(message = "변경할 별명을 입력해 주세요.")
     private String nickName;
 
-    @NotBlank(message = "변경할 비밀번호를 입력해 주세요.")
     private String password;
+
+    /* 빌더를 직접 구현해보자 */
+    public static class Builder {
+        /* 선택 매개변수 */
+        private String name;
+        private String nickName;
+        private String password;
+        /*           */
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder nickName(String nickName) {
+            this.nickName = nickName;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserUpdateRequest build() {
+            return new UserUpdateRequest(this);
+        }
+    }
+
+    private UserUpdateRequest(Builder builder) {
+        name = builder.name;
+        nickName = builder.nickName;
+        password = builder.password;
+    }
 }
