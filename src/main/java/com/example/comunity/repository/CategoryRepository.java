74eec55a55d519b5c3f7a53685a929e6f8983b3e
shinjunseok <sjsage522@gmail.com>
2021-08-03 15:08:14 +0900

    update : CategoryRepository
    메소드 매개변수 이름 변경

--- src/main/java/com/example/comunity/repository/CategoryRepository.java
+++ src/main/java/com/example/comunity/repository/CategoryRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Optional<Category> findByCategoryName(CategoryName categoryName);
+    Optional<Category> findByCategoryName(CategoryName name);
 }
